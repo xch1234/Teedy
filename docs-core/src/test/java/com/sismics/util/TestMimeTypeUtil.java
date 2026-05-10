2961b33713d0e3de53a3f957991b4a44025fedc2
xch1234 <2305645889@qq.com>
2026-05-11 01:06:02 +0800

    Generate project documentation and reports using Maven Site Plugin
    - Added project-reports.html for an overview of generated reports including CPD, PMD, Surefire Report, and JaCoCo Aggregate.
    - Created scm.html detailing source code management with Git, including access instructions for anonymous and developer access.
    - Introduced summary.html providing project summary and information about the project organization and build details.
    - Implemented surefire-report.html to present test results, including a summary of tests, errors, failures, and detailed test case information.

--- docs-core/src/test/java/com/sismics/util/TestMimeTypeUtil.java
+++ docs-core/src/test/java/com/sismics/util/TestMimeTypeUtil.java
@@ -1,13 +1,14 @@
 package com.sismics.util;
 
-import com.sismics.BaseTest;
-import com.sismics.util.mime.MimeType;
-import com.sismics.util.mime.MimeTypeUtil;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import com.sismics.BaseTest;
+import com.sismics.util.mime.MimeType;
+import com.sismics.util.mime.MimeTypeUtil;
 
 /**
  * Test of the utilities to check MIME types.
@@ -68,5 +69,21 @@ public class TestMimeTypeUtil extends BaseTest {
         // Detect MP4 files
         path = Paths.get(getResource(FILE_MP4).toURI());
         Assert.assertEquals(MimeType.VIDEO_MP4, MimeTypeUtil.guessMimeType(path, FILE_MP4));
+    }
+
+    @Test
+    public void getFileExtensionTest() {
+        Assert.assertEquals("zip", MimeTypeUtil.getFileExtension(MimeType.APPLICATION_ZIP));
+        Assert.assertEquals("gif", MimeTypeUtil.getFileExtension(MimeType.IMAGE_GIF));
+        Assert.assertEquals("jpg", MimeTypeUtil.getFileExtension(MimeType.IMAGE_JPEG));
+        Assert.assertEquals("png", MimeTypeUtil.getFileExtension(MimeType.IMAGE_PNG));
+        Assert.assertEquals("pdf", MimeTypeUtil.getFileExtension(MimeType.APPLICATION_PDF));
+        Assert.assertEquals("odt", MimeTypeUtil.getFileExtension(MimeType.OPEN_DOCUMENT_TEXT));
+        Assert.assertEquals("docx", MimeTypeUtil.getFileExtension(MimeType.OFFICE_DOCUMENT));
+        Assert.assertEquals("txt", MimeTypeUtil.getFileExtension(MimeType.TEXT_PLAIN));
+        Assert.assertEquals("csv", MimeTypeUtil.getFileExtension(MimeType.TEXT_CSV));
+        Assert.assertEquals("mp4", MimeTypeUtil.getFileExtension(MimeType.VIDEO_MP4));
+        Assert.assertEquals("webm", MimeTypeUtil.getFileExtension(MimeType.VIDEO_WEBM));
+        Assert.assertEquals("bin", MimeTypeUtil.getFileExtension("application/unknown"));
     }
 }
